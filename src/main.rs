57ec5848ffbf8ca693e6f9243c433e57a8b838be
Yoru-cyber <carlosmendez170210@gmail.com>
2025-01-17 00:26:24 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::env;
+fn main() {
+    let args: Vec<String> = env::args().skip(0).collect();
+    let mut no_newline: bool = false;
+    if args.len() <= 1 {
+        return;
+    }
+    for arg in args {
+        match arg.as_str() {
+            "-n" => no_newline = true,
+            _ => print!("{} ", arg),
+        }
+    }
+    if no_newline == true {
+        print!("");
+        return;
+    }
+    print!("\n");
+    return;
+}
